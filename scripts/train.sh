9520b8e526f962f20920c90cf7bd1fa339b8b104
qqcat <jden4524@gmail.com>
2026-02-28 15:21:53 +1100

    exp with lora

--- scripts/train.sh
+++ scripts/train.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 source attn_ft/bin/activate
 accelerate launch --multi_gpu src/attn_ft/train.py --config configs/qwen3_vl_8b_attn_ft_ce.yaml
-accelerate launch --multi_gpu src/attn_ft/train.py --config configs/qwen3_vl_8b_attn_ft_vacuum.yaml
+# accelerate launch --multi_gpu src/attn_ft/train.py --config configs/qwen3_vl_8b_attn_ft_vacuum.yaml
